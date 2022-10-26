35e774eef85c2b89617129de76b0a14748eaffbf
Dibal <abdulkarimdibal01@gmail.com>
2022-10-26 03:51:24 -0700

    C program that prints exactly

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,12 @@
+#include <stdio.h>                                                                                                                
+                                                                                                                                  
+/**                                                                                                                               
+ *  * main -Entry point                                                                                                              
+ *  *                                                                                                                                
+ *  * Return: Always 0 (Success)                                                                                                     
+ *  */                                                                                                                               
+int main(void)                                                                                                                    
+{                                                                                                                                 
+	        puts("\"programming is the like building a miltilingual puzzle");                                                         
+		        return (0);                                                                                                               
+}                      
